b39716a137f7b1765c71f828093b54fd63dc4df5
simonerocchi <s.rocchi@mind-informatica.com>
2021-06-13 22:33:17 +0200

    workflow

--- gdr-client/src/environments/environment.prod.ts
+++ gdr-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiurl: 'http://localhost/api/v1',
-  wsurl: 'ws://localhost/api/v1/ws'
+  apiurl: 'http://' + window.location.hostname + ":" + window.location.port + '/api/v1',
+  wsurl: 'ws://' + window.location.hostname + ":" + window.location.port + '/api/v1/ws'
 };
