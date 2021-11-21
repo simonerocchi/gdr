a5967b59b322328aa443a7357d41e65c97e21ff6
simonerocchi <s.rocchi@mind-informatica.com>
2021-11-21 15:52:11 +0100

    fix: gorm log on error

--- gdr-server/internal/configuration/configuration.go
+++ gdr-server/internal/configuration/configuration.go
@@ -59,10 +59,10 @@ func Create(pgDsn string, production bool) (*Data, error) {
 		gormConfig.Logger = logger.New(
 			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 			logger.Config{
-				SlowThreshold:             time.Second, // Slow SQL threshold
-				LogLevel:                  logger.Info, // Log level
-				IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-				Colorful:                  true,        // Enable color
+				SlowThreshold:             time.Second,  // Slow SQL threshold
+				LogLevel:                  logger.Error, // Log level
+				IgnoreRecordNotFoundError: true,         // Ignore ErrRecordNotFound error for logger
+				Colorful:                  true,         // Enable color
 			},
 		)
 	}
